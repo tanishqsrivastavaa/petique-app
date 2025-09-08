db06f56c493ff80fc71bf6f43275878d12b14054
Tanishq Srivastava <tanishq280205@gmail.com>
2025-09-08 18:01:55 +0530

    switching branches now

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from app.api.v1.users import router as users_router
+from app.api.v1.pets import router as pets_router
 from fastapi import FastAPI
 
 app = FastAPI()
 app.include_router(users_router,prefix="/api/v1",tags=["users"])
+app.include_router(pets_router,prefix="/api/v1",tags=["pets"])
