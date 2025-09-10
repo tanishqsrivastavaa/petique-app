7187d0e5a6d55d9abef3623d321ca545d936a6ba
Tanishq Srivastava <tanishq280205@gmail.com>
2025-09-10 11:36:47 +0530

    main.py for testing api endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
 from app.api.v1.users import router as users_router
 from app.api.v1.pets import router as pets_router
 from fastapi import FastAPI
+from fastapi.security import HTTPBearer, OAuth2PasswordBearer
+
+bearer_scheme = HTTPBearer()
 
 app = FastAPI()
 app.include_router(users_router,prefix="/api/v1",tags=["users"])
